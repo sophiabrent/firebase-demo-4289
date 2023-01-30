6a0efe40bb476ae6afacbb8bd6cd94732bbc6047
Sophia Brent <sophiabrent@Sophias-MacBook-Air-3.local>
2023-01-30 01:21:02 -0600

    working demo

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyA55AS1jSuYBWegg2Yo56cK7IL3dWGX9XU",
+    authDomain: "cs-4289.firebaseapp.com",
+    databaseURL: "https://cs-4289-default-rtdb.firebaseio.com",
+    projectId: "cs-4289",
+    storageBucket: "cs-4289.appspot.com",
+    messagingSenderId: "424860161475",
+    appId: "1:424860161475:web:41253f7d518906e82a5976",
+    measurementId: "G-2BFC5GTGCM"
+};
+export default config;
